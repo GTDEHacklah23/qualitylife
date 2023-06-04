687e9096f3ee1dba9596ce968cb5f73613949266
piman51277 <piman.dev@gmail.com>
2023-06-04 03:48:11 -0400

    add event/update

--- server/api/event/routes.ts
+++ server/api/event/routes.ts
@@ -4,4 +4,7 @@ const router = Router();
 import newHandler from "./new";
 router.post("/new", newHandler);
 
+import updateHandler from "./update";
+router.post("/update", updateHandler);
+
 export default router;
