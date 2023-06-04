93860ad58439e2cf3e03aca819a2226bd5809047
piman51277 <piman.dev@gmail.com>
2023-06-04 03:10:32 -0400

    create event schema

---
+++ server/schema/Event.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const EventSchema = new mongoose.Schema({
+  id: String,
+  image: String,
+  name: String,
+  description: String,
+  startTimestamp: Number,
+  endTimestamp: Number,
+  location: String,
+  zipCode: String,
+  author: String,
+  tags: [String],
+  attendees: [String],
+});
+
+export const Event = mongoose.model("Event", EventSchema, "Event");
