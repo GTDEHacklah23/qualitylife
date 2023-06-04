a4eff1c43a7a04b82bc960549c2026d01166091c
piman51277 <piman.dev@gmail.com>
2023-06-04 02:02:35 -0400

    add email and zipcode requirements to register

--- server/api/account/new.ts
+++ server/api/account/new.ts
@@ -3,6 +3,8 @@ import { handler } from "../handler";
 import { UserAuth } from "../../schema/UserAuth";
 import { saltshaker } from "../../auth/saltshaker";
 import { hashPassword } from "../../auth/hashPassword";
+import { lookup } from "zipcodes";
+import { UserMeta } from "../../schema/UserMeta";
 
 //Yeah no way I'm writing that regex myself
 //https://stackoverflow.com/a/12155517
@@ -13,16 +15,26 @@ const schema = Joi.object({
     .max(30)
     .pattern(/^((?=(.*[\d0-9@&#$?%!|(){}[\]]){2,})(?=(.*[a-zA-Z]){2,}).{8,})$/)
     .required(),
+  email: Joi.string().email().required(),
+  zipCode: Joi.number().integer().min(10000).max(99999).required(),
 });
 
 export default handler(schema, async (req, res, parsed) => {
-  const { username, password } = parsed;
+  const { username, password, email, zipCode } = parsed;
 
   //check if the username is taken
   const result = await UserAuth.findOne({ username }).exec();
 
   if (result) {
     res.status(409).json({ error: "409 - Conflict" });
+    return;
+  }
+
+  //check if the zip code is valid
+  const zipInfo = lookup(parseInt(zipCode));
+
+  if (!zipInfo) {
+    res.status(400).json({ error: "400 - Invalid Request" });
     return;
   }
 
@@ -40,6 +52,17 @@ export default handler(schema, async (req, res, parsed) => {
   });
 
   await newUser.save();
+
+  //create a new user meta
+  const newUserMeta = new UserMeta({
+    username,
+    email,
+    profileImage: "",
+    dateJoined: Date.now(),
+    zipCode,
+  });
+
+  await newUserMeta.save();
 
   res.status(200).json({ username });
 });
