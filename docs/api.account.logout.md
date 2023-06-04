c99566551c6350a8e12d7e4e572499fdee942c4f
piman51277 <piman.dev@gmail.com>
2023-06-03 21:30:34 -0400

    add logout endpoint

---
+++ docs/api.account.logout.md
@@ -0,0 +1,17 @@
+# POST /api/account/logout
+
+Log out of an existing account
+
+## Parameters
+
+none
+
+## Validation
+
+none
+
+## Response
+
+### 200 OK
+
+User logged out
--- server/api/account/routes.ts
+++ server/api/account/routes.ts
@@ -7,4 +7,7 @@ router.post("/new", newHandler);
 import loginHandler from "./login";
 router.post("/login", loginHandler);
 
+import logoutHandler from "./logout";
+router.post("/logout", logoutHandler);
+
 export default router;
