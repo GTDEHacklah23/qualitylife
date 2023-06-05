29f9fca2eca9c702830e6bee065746a37372d386
piman51277 <piman.dev@gmail.com>
2023-06-04 23:49:24 -0400

    redirect to to forum page if already logged in

--- server/index.ts
+++ server/index.ts
@@ -8,18 +8,18 @@ import apiRouter from "./api/routes";
 app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
-  res.render("index.njk");
+  res.render("index.njk", { user: req.session.username || "none" });
 });
 
 app.get("/signup", (req, res) => {
-  res.render("signup.njk");
+  res.render("signup.njk", { user: req.session.username || "none" });
 });
 app.get("/leaderboard", (req, res) => {
   res.render("leaderboard.njk");
 });
 
 app.get("/login", (req, res) => {
-  res.render("login.njk");
+  res.render("login.njk", { user: req.session.username || "none" });
 });
 
 app.get("/newpost", (req, res) => {
